400ce1a8d24756116214a70ea75a3de7efbadd0d
Ashritta <ashritta@gmail.com>
2022-12-08 22:06:12 +0800

    Minor changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 $(document).ready(function () {
     $("#encrypt").click(function (e) {
         e.preventDefault()
-        function rot13encrypt(form) {
+        function rot13encrypt(str) {
             var newstr = "";
-            for (var i = 0; i < form.length; i++) {
-                var charCode = form.charCodeAt(i);
+            for (var i = 0; i < str.length; i++) {
+                var charCode = str.charCodeAt(i);
                 if (charCode > 96 && charCode < 110) {
                     newstr += String.fromCharCode(charCode + 13);
                 }
@@ -13,14 +13,13 @@ $(document).ready(function () {
                     newstr += String.fromCharCode(charCode - 13);
                 }
                 else {
-                    newstr += form[i];
+                    newstr += str[i];
                 }
             }
             return newstr;
         }
         var text = $("#text").val();
-        var lowerCase = text.toLowerCase();
-        var outputmsg = rot13encrypt(lowerCase);
+        var outputmsg = rot13encrypt(text.toLowerCase());
         $("#output").html(outputmsg);
     })
 })
@@ -28,10 +27,10 @@ $(document).ready(function () {
 $(document).ready(function () {
     $("#decrypt").click(function (e) {
         e.preventDefault()
-        function rot13decrypt(form) {
+        function rot13decrypt(str) {
             var newstr = "";
-            for (var i = 0; i < form.length; i++) { //iterates through characters in str
-                var charCode = form.charCodeAt(i); // sets new var charCode to each character in str
+            for (var i = 0; i < str.length; i++) { //iterates through characters in str
+                var charCode = str.charCodeAt(i); // sets new var charCode to each character in str
                 if (charCode > 96 && charCode < 110) { // if the character is in first 13 of alpha
                     newstr += String.fromCharCode(charCode - 13); // set newstr equal to the character + 13
                 }
@@ -39,14 +38,13 @@ $(document).ready(function () {
                     newstr += String.fromCharCode(charCode + 13);
                 }
                 else {
-                    newstr += form[i];
+                    newstr += str[i];
                 }
             }
             return newstr;
         }
         var text = $("#text").val();
-        var lowerCase = text.toLowerCase();
-        var outputmsg = rot13decrypt(lowerCase);
+        var outputmsg = rot13decrypt(text.toLowerCase());
         $("#output").html(outputmsg);
     })
 })
