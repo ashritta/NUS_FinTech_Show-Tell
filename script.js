347dfb1353aca49e44fd4a6935e89d3fd020cb1e
Ashritta <ashritta@gmail.com>
2022-12-08 23:20:43 +0800

    Minor changes

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ $(document).ready(function () {
         e.preventDefault()
         function rot13decrypt(str) {
             var newstr = "";
-            for (var i = 0; i < str.length; i++) { //iterates through characters in str
-                var charCode = str.charCodeAt(i); // sets new var charCode to each character in str
-                if (charCode > 96 && charCode < 110) { // if the character is in first 13 of alpha
-                    newstr += String.fromCharCode(charCode - 13); // set newstr equal to the character + 13
-                }
-                else if (charCode > 109 && charCode < 123) { // if the character is in last 13 of alpha
+            for (var i = 0; i < str.length; i++) {
+                var charCode = str.charCodeAt(i);
+                if (charCode > 96 && charCode < 110) {
                     newstr += String.fromCharCode(charCode + 13);
+                }
+                else if (charCode > 109 && charCode < 123) {
+                    newstr += String.fromCharCode(charCode - 13);
                 }
                 else {
                     newstr += str[i];
