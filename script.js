a7809e06d2e126b65f1f10df7b6eee0e6bd58f19
Ashritta <ashritta@gmail.com>
2022-12-09 12:35:48 +0800

    Custom cipher update

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 
-$(document).ready(function () {
-    $("#rot13encrypt").click(function (e) {
-        e.preventDefault()
-        function rot13encrypt(str) {
-            var newstr = "";
-            for (var i = 0; i < str.length; i++) {
-                var charCode = str.charCodeAt(i);
-                if (charCode > 96 && charCode < 110) {
-                    newstr += String.fromCharCode(charCode + 13);
-                }
-                else if (charCode > 109 && charCode < 123) {
-                    newstr += String.fromCharCode(charCode - 13);
-                }
-                else {
-                    newstr += str[i];
-                }
+$("#rot13encrypt").click(function (e) {
+    e.preventDefault()
+    function rot13encrypt(str) {
+        var newstr = "";
+        for (var i = 0; i < str.length; i++) {
+            var charCode = str.charCodeAt(i);
+            if (charCode > 96 && charCode < 110) {
+                newstr += String.fromCharCode(charCode + 13);
             }
-            return newstr;
+            else if (charCode > 109 && charCode < 123) {
+                newstr += String.fromCharCode(charCode - 13);
+            }
+            else {
+                newstr += str[i];
+            }
         }
-        var text = $("#msgtext").val();
-        var outputmsg = rot13encrypt(text.toLowerCase());
-        $("#rot13output").html(outputmsg);
-    })
+        return newstr;
+    }
+    var text = $("#msgtext").val();
+    var outputmsg = rot13encrypt(text.toLowerCase());
+    $("#rot13output").html(outputmsg);
 })
 
 
-$(document).ready(function () {
-    $("#custencrypt").click(function (e) {
-        e.preventDefault()
-        function custencrypt(str, shift) {
-            var newstr = "";
-            for (var i = 0; i < str.length; i++) {
-                var charCode = str.charCodeAt(i);
-                if (charCode > 96 && charCode < 110) {
-                    newstr += String.fromCharCode(charCode + shift);
-                }
-                else if (charCode > 109 && charCode < 123) {
-                    newstr += String.fromCharCode(charCode - shift);
-                }
-                else {
-                    newstr += str[i];
-                }
+$("#custencrypt").click(function (e) {
+    e.preventDefault()
+    function custencrypt(str, num) {
+        num = num % 26;
+        var newstr = "";
+        for (var i = 0; i < str.length; i++) {
+            var charCode = str.charCodeAt(i);
+            if (charCode > 96 && charCode < 110) {
+                newstr += String.fromCharCode(charCode + num);
             }
-            return newstr;
+            else if (charCode > 109 && charCode < 123) {
+                newstr += String.fromCharCode(charCode - num);
+            }
+            else {
+                newstr += str[i];
+            }
         }
-        var text = $("#msgtext").val();
-        var shift = $("#msgshift").parseInt();
-        var outputmsg = "custencrypt(text.toLowerCase(), shift)";
-        $("#custoutput").html(outputmsg);
-    })
+        return newstr;
+    }
+    var text = $("#msgtext").val();
+    var shift = $("#msgshift").val();
+    var outputmsg = custencrypt(text.toLowerCase(), parseInt(shift));
+    $("#custoutput").html(outputmsg);
 })
+
 
